d8872e497d6a02350f50eee663fccb9d3c51599b
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-03-20 10:21:05 -0400

    initial commit

---
+++ src/main/java/com/careydevelopment/demo/twitterlogin/controller/TwitterLoginController.java
@@ -0,0 +1,16 @@
+package com.careydevelopment.demo.twitterlogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TwitterLoginController {
+	
+	//starting page for Twitter login demo
+	@RequestMapping("/twitterLogin")
+	public String twiterLogin(Model model) {
+		return "twitterLogin";
+	}
+
+}
