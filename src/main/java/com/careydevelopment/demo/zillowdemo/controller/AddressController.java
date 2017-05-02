3c0bb61cf5e2e0b1c788bb2994483bfaad18f589
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-05-02 18:36:37 -0400

    Added additional demos

---
+++ src/main/java/com/careydevelopment/demo/zillowdemo/controller/AddressController.java
@@ -0,0 +1,53 @@
+package com.careydevelopment.demo.zillowdemo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.careydevelopment.demo.zillowdemo.model.Address;
+import com.careydevelopment.demo.zillowdemo.model.ZillowSearchResults;
+import com.careydevelopment.demo.zillowdemo.util.ZillowApiHelper;
+
+
+
+@Controller
+public class AddressController {
+
+	//starting page for zillow api demo
+	@RequestMapping(value = "/enterAddress", method=RequestMethod.GET)
+	public String enterAddress(Model model) {
+		//instantiate an empty address object
+		Address address = new Address();
+		
+		//put the object in the model
+		model.addAttribute("address", address);
+		
+		//get out
+		return "enterAddress";
+	}
+
+	
+	@RequestMapping(value = "/enterAddress", method=RequestMethod.POST)
+	public String formSubmit(@Valid Address address, BindingResult bindingResult, Model model) {
+		//check for errors
+		if (bindingResult.hasErrors()) {
+            return "enterAddress";
+        }
+		
+		//get the search results object
+		ZillowSearchResults results = ZillowApiHelper.getSearchResults(address);
+		
+		//put it in the model
+		model.addAttribute("results", results);
+		
+		//add the address to the model as well
+		model.addAttribute("address", address);
+		
+		//get out
+		return "showZillowResults";
+	}
+}
