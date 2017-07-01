c520ba1442036ad16d863413cce548ee5ed01a15
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-07-01 07:08:57 -0400

    Added Highcharts demo

---
+++ src/main/java/com/careydevelopment/demo/highcharts/controller/ChartController.java
@@ -0,0 +1,39 @@
+package com.careydevelopment.demo.highcharts.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ChartController {
+
+    @RequestMapping(value = "/chart", method=RequestMethod.GET)
+    public String chart(Model model) {
+        
+        //first, add the regional sales
+        Integer northeastSales = 17089;
+        Integer westSales = 10603;
+        Integer midwestSales = 5223;
+        Integer southSales = 10111;
+        
+        model.addAttribute("northeastSales", northeastSales);
+        model.addAttribute("southSales", southSales);
+        model.addAttribute("midwestSales", midwestSales);
+        model.addAttribute("westSales", westSales);
+        
+        //now add sales by lure type
+        List<Integer> inshoreSales = Arrays.asList(4074, 3455, 4112);
+        List<Integer> nearshoreSales = Arrays.asList(3222, 3011, 3788);
+        List<Integer> offshoreSales = Arrays.asList(7811, 7098, 6455);
+        
+        model.addAttribute("inshoreSales", inshoreSales);
+        model.addAttribute("nearshoreSales", nearshoreSales);
+        model.addAttribute("offshoreSales", offshoreSales);
+        
+        return "chart";
+    }
+}
