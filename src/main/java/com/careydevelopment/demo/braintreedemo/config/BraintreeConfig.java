06c63593800a5d9abe3bc74a678cc697deb5d0d0
careyb <careyb@LAPTOP-DJERTLQ2>
2018-04-23 07:15:16 -0400

    Fixed a problem with config

--- src/main/java/com/careydevelopment/demo/braintreedemo/config/BraintreeConfig.java
+++ src/main/java/com/careydevelopment/demo/braintreedemo/config/BraintreeConfig.java
@@ -1,10 +1,16 @@
 package com.careydevelopment.demo.braintreedemo.config;
 
-import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.braintreegateway.BraintreeGateway;
 import com.careydevelopment.demo.braintreedemo.util.BraintreeGatewayFactory;
@@ -17,22 +23,45 @@ public class BraintreeConfig {
 
     @Bean
     public BraintreeGateway getBraintreeGateway() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File configFile = new File(classLoader.getResource(CONFIG_FILENAME).getFile());
-
+        Map<String,String> configMap = getMap();        
+        
         BraintreeGateway gateway = null;
         
         try {
-            if(configFile.exists() && !configFile.isDirectory()) {
-                gateway = BraintreeGatewayFactory.fromConfigFile(configFile);
-            } else {
-                gateway = BraintreeGatewayFactory.fromConfigMapping(System.getenv());
-            }            
-        } catch (NullPointerException e) {
-            System.err.println("Could not load Braintree configuration from config file or system environment.");
+            gateway = BraintreeGatewayFactory.fromConfigMapping(configMap);
+        } catch (Exception e) {
+            e.printStackTrace();
             System.exit(1);
         }
         
         return gateway;
+    }
+    
+    
+    /**
+     * Creates the map that we'll use to initialize the gateway
+     * Reads values from the config file
+     */
+    private Map<String,String> getMap() {
+        Map<String,String> map = new HashMap<String,String>();
+        
+        try {
+            //get the input stream from the file in the resources folder
+            Resource resource = new ClassPathResource(CONFIG_FILENAME);
+            InputStream is = resource.getInputStream();
+            
+            //create a Properties object of key/value pairs
+            Properties properties = new Properties();
+            properties.load(is);
+            
+            //Special thanks to Java 8
+            map.putAll(properties.entrySet().stream()
+                    .collect(Collectors.toMap(e -> e.getKey().toString(), e -> e.getValue().toString())));            
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        return map;
     }
 }
